57eac431ce356c12bb73b4bab829fb80572e76f8
DalerUZB <dalir2001mail.com@gmail.com>
2022-07-06 15:12:36 +0500

    todo-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import { v4 } from 'uuid';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([])
+  const [search, setSearch] = useState([])
+  const inputRef = useRef(0)
+  const id = v4();
+
+  const saveTodoFunc = () => {
+    const newTodo = {
+      id: id,
+      title: inputRef.current.value
+    }
+    setTodos([...todos, newTodo])
+    inputRef.current.value = ""
+
+  }
+
+  const listinerEnter = (event) => {
+    if (event.key === "Enter") {
+      saveTodoFunc()
+    }
+  }
+
+  const serachFunc = (target) => {
+    setSearch(todos.filter((elem) => elem.title.includes(target.value)))
+  }
+
+  const removeItems = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='forFlex'>
+        <input
+          placeholder='To Write'
+          onKeyUp={(event) => listinerEnter(event)}
+          ref={inputRef}
+          className="input"
+        />
+
+        <button
+          onClick={() => saveTodoFunc()}
+          className="btn"
+        >saqlash</button>
+
+      </div>
+
+
+
+      <ul>
+        {todos.map((item) =>
+          <li
+
+            className='li'
+            key={item.id}
+            onClick={() => removeItems(item.id)}
+          >
+            {item.title}
+          </li>)}
+      </ul>
+
+
+      <div>
+        <input
+          onKeyUp={
+
+            ({ target }) => serachFunc(target)}
+          placeholder='Search'
+          className="input"
+
+        />
+        <ul>
+          {search.map((item) =>
+            <li
+              className='li'
+              key={item.id}>
+              {item.title}
+            </li>)}
+        </ul>
+      </div>
+    </div >
   );
 }
 
